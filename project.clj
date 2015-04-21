f74697800dfaa4efcc7ecd0b44ff18e0761e44d2
luxbock <opieppo@gmail.com>
2015-04-21 11:07:42 +0700

    Fixes in project.clj for deployment

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject pr-foobar "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Pretty print debugging macros for Clojure."
+  :url "https://github.com/luxbock/pr-foobar"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git", :url "https://github.com/luxbock/pr-foobar"}
+  :deploy-repositories [["clojars" {:creds :gpg}]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [table "0.4.0"]])
